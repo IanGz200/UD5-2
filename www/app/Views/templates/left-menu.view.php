03bede755888f7ce93d117db1f2769b1b955b035
Rafael González Centeno <rgcenteno@edu.xunta.gal>
2024-11-04 18:20:18 +0100

    Entradas menú lateral

--- www/app/Views/templates/left-menu.view.php
+++ www/app/Views/templates/left-menu.view.php
@@ -62,5 +62,44 @@
                   </ul>
               </li>
           </ul>
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+              <!-- Add icons to the links using the .nav-icon class
+                 with font-awesome or any other icon font library -->
+              <li class="nav-item <?php echo in_array($_SERVER['REQUEST_URI'], [$_ENV['host.folder'].'usuarios', $_ENV['host.folder'].'usuarios-order-by-salar', $_ENV['host.folder'].'usuarios-estandar', $_ENV['host.folder'].'usuarios-carlos']) ? 'menu-open' : '';?>">
+                  <a href="#" class="nav-link">
+                      <i class="nav-icon fa fa-user"></i>
+                      <p>
+                          DB Sencillos
+                          <i class="right fas fa-angle-left"></i>
+                      </p>
+                  </a>
+                  <ul class="nav nav-treeview">
+                      <li class="nav-item">
+                          <a href="<?php echo $_ENV['host.folder']; ?>usuarios" class="nav-link <?php echo $_SERVER['REQUEST_URI'] === $_ENV['host.folder'].'usuarios' ? 'active' : ''; ?>">
+                              <i class="fas fa-user-friends nav-icon"></i>
+                              <p>Todos usuarios</p>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="<?php echo $_ENV['host.folder']; ?>usuarios-order-by-salar" class="nav-link <?php echo $_SERVER['REQUEST_URI'] === $_ENV['host.folder'].'usuarios-order-by-salar' ? 'active' : ''; ?>">
+                              <i class="fas fa-user-plus nav-icon"></i>
+                              <p>Usuarios por salario</p>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="<?php echo $_ENV['host.folder']; ?>usuarios-estandar" class="nav-link <?php echo $_SERVER['REQUEST_URI'] === $_ENV['host.folder'].'usuarios-estandar' ? 'active' : ''; ?>">
+                              <i class="fas fa-user-alt nav-icon"></i>
+                              <p>Estándar</p>
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a href="<?php echo $_ENV['host.folder']; ?>usuarios-carlos" class="nav-link <?php echo $_SERVER['REQUEST_URI'] === $_ENV['host.folder'].'usuarios-carlos' ? 'active' : ''; ?>">
+                              <i class="fas fa-user-md nav-icon"></i>
+                              <p>Carlos</p>
+                          </a>
+                      </li>
+                  </ul>
+              </li>
+          </ul>
       </nav>
       <!-- /.sidebar-menu -->
